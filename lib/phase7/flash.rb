c68283e55f12ca58754a2014c74523fabdffb6a6
charles <charles@appacademy.io>
2015-04-22 18:07:45 -0400

    Start basing the flash on cookie.

--- lib/phase7/flash.rb
+++ lib/phase7/flash.rb
@@ -1,21 +1,18 @@
 require_relative '../phase6/controller_base'
 module Phase7
   class Flash < Phase6::ControllerBase
-    attr_accessor :flash
-    def initialize(key, val)
-      @flash[key] = val
-    end
-
-    def called?
-
-    end
-
-    def [](key)
-      flash[key]
+    def initialize(req)
+      @req = req
+      flash = @req.cookies.select {|cookie| cookie.name == 'flash' }
+      if flash.empty?
+        @flash_hash = {}
+      else
+        @flash_hash = JSON.parse(the_cookie.first.value)
+      end
     end
 
     def now
-
+      res.cookies << flash
     end
   end
 end
